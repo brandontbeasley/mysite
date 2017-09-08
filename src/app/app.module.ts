402a808796d546ca56791868cf1ccd2b73824fca
Brandon Beasley <bbeasley@bp-3.com>
2017-09-07 19:28:05 -0500

    add routing and navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,26 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ResumeComponent } from './resume/resume.component';
+import { HomeComponent } from './home/home.component';
+import { RouterModule } from '@angular/router';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ResumeComponent,
+    HomeComponent,
+    NavbarComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([
+      {  component: ResumeComponent, path: 'resume' },
+      {  component: HomeComponent, path: 'home' },
+      {  path: '**', pathMatch: 'full', redirectTo: 'home' }
+    ])
+
   ],
   providers: [],
   bootstrap: [AppComponent]
